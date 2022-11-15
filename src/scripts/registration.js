26b402b16f4bb6fe1f3ad015a5490cf04f41ffcf
vke-csekar <93593553+vke-csekar@users.noreply.github.com>
2022-11-15 19:18:04 +0530

    no message

--- src/scripts/registration.js
+++ src/scripts/registration.js
@@ -81,3 +81,17 @@ $(function () {
         console.log('Binding applied for Registration Component');
     }       
 });
+
+function parseJwt(token) {
+    try {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        return JSON.parse(jsonPayload);
+    }
+    catch (e) {
+        throw new Error('Invalid token');
+    }
+}
