3a14c146d50bd3bc507a368647d05ec721851edf
vke-csekar <93593553+vke-csekar@users.noreply.github.com>
2022-11-15 17:20:42 +0530

    no message

--- src/scripts/registration.js
+++ src/scripts/registration.js
@@ -1,9 +1,22 @@
+function RegistrationViewModel () {
+    var self = this;
+    self.firstName = ko.observable();
+    self.registrationFormSubmit = function (formRootElement) {
+        console.log(self.firstName());
+    };
+};
+
 $(function () {
-    
+    var registrationFormRootElement = document.getElementById('registrationcomponent');
+    if (registrationFormRootElement) {
+        var componentModel = new RegistrationViewModel();
+        ko.applyBindings(componentModel, registrationFormRootElement);
+        console.log('Binding applied for Registration Component');
+    }       
 });
 
-$('#registrationForm').submit(function (e) {
-    e.preventDefault();
-    console.log('Form submitting...');
-    console.log($('#first-name').val());
-});
+//$('#registrationForm').submit(function (e) {
+//    e.preventDefault();
+//    console.log('Form submitting...');
+//    console.log($('#first-name').val());
+//});
